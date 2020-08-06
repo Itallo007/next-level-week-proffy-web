86ffe2fc1f1934a8351583f4f44c2dec2fe9695b
itallo007 <italosantos.oficial2000@gmail.com>
2020-08-06 11:26:36 -0300

    Busca o total de connections da Api

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import logoImg from "../../assets/images/logo.svg";
@@ -8,10 +8,25 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeratIcon from "../../assets/images/icons/purple-heart.svg";
 
+import api from "../../services/api";
+
 import "./styles.css";
 
 
 function Landing() {
+
+	const [totalConnections, setTotalConnections] = useState(0);
+
+	useEffect(() => {
+		loadTotalConnections();
+	}, []);
+
+	function loadTotalConnections() {
+		api.get('/connections').then(response => {
+			setTotalConnections(response.data.total);
+		});
+	}
+
 	return (
 		<div id="page-landing">
 			<div id="page-landing-content" className="container">
@@ -38,7 +53,7 @@ function Landing() {
 				</div>
 
 				<span className="total-connections">
-					Total de 200 conexões já realizadas <img src={purpleHeratIcon} alt="Coração roxo"/>
+					Total de {totalConnections} conexões já realizadas <img src={purpleHeratIcon} alt="Coração roxo"/>
 				</span>
 			</div>
 		</div>
